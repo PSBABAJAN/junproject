566aba2aae0c0602b2cc58da45fbe3811eccf542
PSBABAJAN <babajanshaik474@gmail.com>
2026-02-12 20:36:05 +0530

    phython.code

---
+++ Devops.java
@@ -0,0 +1,16 @@
+class Test
+{
+	public static void main(String[]args)
+	{
+		int number = 88;
+		if (number %2 == 0);
+		{
+			System.out.println(number +"is even");
+		}
+		else
+		{
+			System.out.println(number +"is odd");
+		}
+	}
+}
+
